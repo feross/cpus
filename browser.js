2e9022fc7a9d134ab86704bbb374e7ecc36c3142
Feross Aboukhadijeh <feross@feross.org>
2018-03-15 10:38:31 -0700

    style

--- browser.js
+++ browser.js
@@ -2,7 +2,11 @@ module.exports = function cpus () {
   var num = navigator.hardwareConcurrency || 1
   var cpus = []
   for (var i = 0; i < num; i++) {
-    cpus.push({ model: '', speed: 0, times: { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 } })
+    cpus.push({
+      model: '',
+      speed: 0,
+      times: { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 }
+    })
   }
   return cpus
 }
